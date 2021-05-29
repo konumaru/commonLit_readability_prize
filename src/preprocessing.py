4468ebf5cd8f54e15a19079239b5113472f15b8c
konumaru <konumaru1022@gmail.com>
2021-05-29 15:34:16 +0000

    [update] implement preprocessing.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,26 +1,74 @@
+import pathlib
+import re
+from typing import AnyStr
+
+import nltk
 import numpy as np
 import pandas as pd
-import torch
-from transformers import (
-    AdamW,
-    BertConfig,
-    BertModel,
-    BertTokenizer,
-    get_linear_schedule_with_warmup,
-)
+import textstat
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from pandarallel import pandarallel
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from utils.common import save_cache, timer
+
+nltk.download("punkt")
+nltk.download("wordnet")
+nltk.download("stopwords")
+
+pandarallel.initialize(progress_bar=True)
+
+DST_DIR = "../data/features/"
+
+
+@save_cache(filepath=f"{DST_DIR}preprocessed_excerpt.pkl", use_cache=True)
+def get_preprocessed_excerpt(src_data: pd.DataFrame) -> pd.DataFrame:
+    def preprocess_excerpt(text: AnyStr):
+        text = re.sub("[^a-zA-Z]", " ", text).lower()
+        text = nltk.word_tokenize(text)  # NOTE: 英文を単語分割する
+        text = [word for word in text if word not in set(stopwords.words("english"))]
+
+        lemma = nltk.WordNetLemmatizer()  # NOTE: 複数形の単語を単数形に変換する
+        text = " ".join([lemma.lemmatize(word) for word in text])
+        return text
+
+    dst_data = src_data["excerpt"].parallel_apply(preprocess_excerpt)
+    return dst_data
+
+
+@save_cache(filepath=f"{DST_DIR}textstats.pkl", use_cache=True)
+def get_textstat(src_data: pd.DataFrame) -> pd.DataFrame:
+    dst_data = pd.DataFrame()
+
+    dst_data = dst_data.assign(
+        excerpt_len=src_data["preprocessed_excerpt"].str.len(),
+        avg_word_len=(
+            src_data["preprocessed_excerpt"]
+            .apply(lambda x: [len(s) for s in x.split()])
+            .map(np.mean)
+        ),
+        char_count=src_data["excerpt"].map(textstat.char_count),
+        word_count=src_data["preprocessed_excerpt"].map(textstat.lexicon_count),
+        sentence_count=src_data["excerpt"].map(textstat.sentence_count),
+        syllable_count=src_data["excerpt"].apply(textstat.syllable_count),
+        smog_index=src_data["excerpt"].apply(textstat.smog_index),
+        automated_readability_index=src_data["excerpt"].apply(
+            textstat.automated_readability_index
+        ),
+        coleman_liau_index=src_data["excerpt"].apply(textstat.coleman_liau_index),
+        linsear_write_formula=src_data["excerpt"].apply(textstat.linsear_write_formula),
+    )
+    return dst_data
 
 
 def main():
-    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-    model = BertModel.from_pretrained("bert-base-uncased")
+    train = pd.read_csv("../data/raw/train.csv", usecols=["id", "excerpt", "target"])
+    train["preprocessed_excerpt"] = get_preprocessed_excerpt(train)
 
-    inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
-    outputs = model(**inputs)
-
-    last_hidden_states = outputs.last_hidden_state
-
-    print(last_hidden_states)
+    _ = get_textstat(train)
 
 
 if __name__ == "__main__":
-    main()
+    with timer("Preprocessing"):
+        main()
